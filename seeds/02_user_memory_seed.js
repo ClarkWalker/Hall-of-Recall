d4633c2f3c0f53f74ca5c83b172032157d199fcb
Clark Walker <303walker@gmail.com>
2017-10-26 15:12:36 -0600

    update

--- seeds/02_user_memory_seed.js
+++ seeds/02_user_memory_seed.js
@@ -7,7 +7,8 @@ exports.seed = function(knex, Promise) {
       return knex('user_memory').insert([
         {id: 1, memory_id: "3", user_id: "2"},
         {id: 2, memory_id: "1", user_id: "3"},
-        {id: 3, memory_id: "2", user_id: "3"}
+        {id: 3, memory_id: "2", user_id: "3"},
+        {id: 3, memory_id: "2", user_id: "1"},
       ]);
     });
 };
